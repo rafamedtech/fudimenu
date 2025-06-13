e473ac8672613ce9d576a62c44877ea91a429406
Rafael <rafamed.tech@gmail.com>
2025-06-12 19:37:27 -0700

    feat: update CategoryItem component to use props for cover image

--- app/components/CategoryItem.vue
+++ app/components/CategoryItem.vue
@@ -4,23 +4,14 @@ interface Props {
   cover?: string;
 }
 
-const { category } = defineProps<Props>();
+const { slug, cover } = defineProps<Props>();
 </script>
 
 <template>
   <NuxtLink :to="`/menu/${slug}`" class="w-full text-center xl:h-[300px]">
     <UCard :ui="{ body: 'p-0 sm:p-0' }">
       <figure class="h-44 shadow-lg md:h-[25rem] lg:h-[300px]">
-        <img :src="category.cover" class="h-full w-full rounded-lg object-cover object-center brightness-75" />
-        <!-- <img
-          :src="cover"
-          densities="x1 x2"
-          format="webp"
-          quality="80"
-          preload
-          loading="lazy"
-          class="h-full w-full rounded-lg object-cover object-center brightness-75"
-        /> -->
+        <img :src="cover" class="h-full w-full rounded-lg object-cover object-center brightness-75" />
       </figure>
     </UCard>
   </NuxtLink>
